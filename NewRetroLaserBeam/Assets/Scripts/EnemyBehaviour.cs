132475462b75106c24aee43dca819278a7b36434
MarnenCaes <lavian.naival@yahoo.fr>
2019-10-14 15:19:23 +0200

    Added Enemies on scene

---
+++ NewRetroLaserBeam/Assets/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(ParticleSystem))]
+public class EnemyBehaviour : MonoBehaviour {
+
+    [SerializeField]float healthPoint = 1;
+    public float moveSpeed = 0.25f;
+    public float damagePoint = 1;
+    public bool isCloseToPlayers;
+    Collider headCollider;
+    Collider bodyCollider;
+    public Camera mainCamera;
+    Animator animator;
+    public float hitTime = 1f;
+    public float hitCooldown = 0;
+    public int[] playersHit = { 0, 0, 0, 0 };
+    public ParticleSystem particleSystem;
+    void Start () {
+        mainCamera = Camera.main;
+        animator = GetComponent<Animator>();
+        particleSystem = GetComponent<ParticleSystem>();
+        headCollider = transform.GetChild(0).GetComponent<Collider>();
+        bodyCollider = transform.GetChild(1).GetComponent<Collider>();
+        EnemyIsActive(true);
+        hitCooldown = hitTime;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if(hitCooldown > 0)
+        {
+            hitCooldown -= Time.deltaTime;
+        }
+        CheckHealth();
+
+	}
+    public void GoToLocation()
+    {
+        transform.position += (mainCamera.transform.position - transform.position).normalized * Time.deltaTime * moveSpeed; 
+    }
+    void CheckHealth()
+    {
+        if (healthPoint <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+    public float DealDamage(float _damage,Collider _col, int _playerId)
+    {
+        //Check if hit on head.
+        if (_col == headCollider)
+        {
+            playersHit[_playerId] = 2;
+        }
+        else
+        {
+            playersHit[_playerId] = 1;
+        }
+        //Check if we can hit
+        if (hitCooldown <= 0)
+        {
+            float totalDamage = 0;
+            for(int i = 0; i < playersHit.Length; i++)
+            {
+                totalDamage += (_damage * playersHit[i]);
+            }
+            ResetPlayersHit();
+            hitCooldown = hitTime;
+            print(totalDamage);
+            particleSystem.Play();
+            return healthPoint -= totalDamage;
+        }
+        else
+        {
+            return 0;
+        }
+
+    }
+    private void ResetPlayersHit()
+    {
+        for(int i = 0; i < playersHit.Length; i++)
+        {
+            playersHit[i] = 0;
+        }
+    }
+    public bool EnemyIsActive(bool _state)
+    {
+        animator.SetBool("isActive", _state);
+        return _state;
+    }
+    public bool EnemyIsClose(bool _state)
+    {
+        animator.SetBool("isClose", _state);
+        return _state;
+    }
+}
--- NewRetroLaserBeam/Assets/Scripts/LaserBehaviour.cs
+++ NewRetroLaserBeam/Assets/Scripts/LaserBehaviour.cs
@@ -10,8 +10,10 @@ public class LaserBehaviour : MonoBehaviour
     LineRenderer laser;
     public Ray ray;
     public RaycastHit hit;
+    public bool laserHit = false;
     public static LaserManager laserManager;
     public bool isShooting = false;
+    public float laserDamage = 0.1f;
     [Range(0, 3)] public int playerId;
     void Awake()
     {
@@ -27,17 +29,25 @@ public class LaserBehaviour : MonoBehaviour
         UpdateLaserRootPosition();
         UpdateLaserPositions();
 #if UNITY_EDITOR
-        /*if (playerId == 0)
+        if (playerId == 0)
         {
             if (Input.GetMouseButtonDown(0))
             {
-                SetLaserActive(!isShooting);
+                SetFalseLaser(!isShooting);
             }
             Vector3 vector = laserManager.mainCamera.ScreenPointToRay(Input.mousePosition).direction;
 
             scopeImage.transform.position = laserManager.mainCamera.transform.position + vector.normalized;
-        } */
+        }
 #endif
+        if (laserHit)
+        {
+            if (isShooting && hit.transform.GetComponent<EnemyBehaviour>())
+            {
+                hit.transform.GetComponent<EnemyBehaviour>().DealDamage(laserDamage, hit.collider, playerId);
+            }
+        }
+
     }
 
     public void UpdateLaserRootPosition()
@@ -51,14 +61,20 @@ public class LaserBehaviour : MonoBehaviour
         ray = laserManager.mainCamera.ScreenPointToRay(laserManager.mainCamera.WorldToScreenPoint(scopeImage.transform.position));
         if (Physics.Raycast(ray, out hit))
         {
+            laserHit = true;
             laser.SetPosition(1, hit.point);
         }
         else
         {
+            laserHit = false;
             laser.SetPosition(1, scopeImage.transform.position);
         }
     }
-
+    public bool SetFalseLaser(bool _state)
+    {
+        isShooting = _state;
+        return laser.enabled = _state;
+    }
     public void SetLaser(ButtonControllerType shootButton)
     {
         if (shootButton.BUTTON_STATE_IS_PRESSED)
