67179a142cb7c4c2667eb9d1fb8dce4e31df9c34
Pierre <haro@netc.fr>
2019-11-05 21:55:58 +0100

    Test com server to client OK

--- NewRetroLaserBeam/Assets/Scripts/Client/ClientManager.cs
+++ NewRetroLaserBeam/Assets/Scripts/Client/ClientManager.cs
@@ -1,12 +1,16 @@
 ﻿using EasyWiFi.ClientBackchannels;
+using EasyWiFi.Core;
+using EasyWiFi.ServerBackchannels;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ClientManager : MonoBehaviour {
 
     public GameObject coinButton;
     float time;
+    public Text text;
     IEnumerator displayCoinPanel()
     {
         yield return new WaitForSeconds(time);
@@ -20,12 +24,14 @@ public class ClientManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        text.text = time.ToString();
 
-    void SpendCoins(FloatClientBackchannel time)
+    }
+
+    void SpendCoins(FloatBackchannelType inputTime)
     {
-        coinButton.SetActive(false);
+        coinButton.SetActive(true);
+        time = inputTime.FLOAT_VALUE;
         StartCoroutine("displayCoinPanel");
     }
 }
